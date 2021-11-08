62cfafd9fa09fda0ef7059c4467c317cfe03fcde
Tashfiq Chowdhury <tashfiqchowdhury@MacBook-Pro.local>
2021-11-08 15:03:12 -0800

    Adding docker stuff

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:latest
+WORKDIR /app
+ADD . /app
+RUN pip install -r requirements.txt
+CMD ["python", "application.py"]
--- application.py
+++ application.py
@@ -42,4 +42,4 @@ def addAppointment():
     return {"appointments": appointments}, 201
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", debug=True)
